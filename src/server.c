7508d6fadbd7361e575d2f773d4bf8d6f6fce5f4
HomeACcessoryKid <homeaccessorykid@gmail.com>
2020-04-12 16:07:14 +0200

    after syncing to a4b8d3e added unique_name to mdns-hostname

--- src/server.c
+++ src/server.c
@@ -3327,7 +3327,14 @@ void homekit_setup_mdns(homekit_server_t *server) {
         return;
     }
 
-    homekit_mdns_configure_init(name->value.string_value, PORT);
+    char unique_name[65]={0};
+    strncpy(unique_name, name->value.string_value, sizeof(unique_name)-6);
+    unique_name[strlen(unique_name)]='-';
+    unique_name[strlen(unique_name)]=server->accessory_id[0];
+    unique_name[strlen(unique_name)]=server->accessory_id[1];
+    unique_name[strlen(unique_name)]=server->accessory_id[3];
+    unique_name[strlen(unique_name)]=server->accessory_id[4];
+    homekit_mdns_configure_init(unique_name, PORT);
 
     // accessory model name (required)
     homekit_mdns_add_txt("md", "%s", model->value.string_value);
